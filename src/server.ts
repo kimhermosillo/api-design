672ac120090d7fce65de21290c083dcc78549d85
Kim Hermosillo <kimhermosillo@yahoo.com>
2024-11-16 16:08:50 -0700

    able to sign in and create user 🔐

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import router from './router'
 import morgan from 'morgan'
 import { protect } from './modules/auth'
+import { createNewUser, signIn } from './handlers/user'
 
 const app = express()
 
@@ -14,6 +15,9 @@ app.get('/', (req, res) => {
     res.json({ user: 'Rager Hermosillo' })
 })
 
+// adding protect below so middleware prevents unauthorized users from routes
 app.use('/api', protect, router)
-
+// no protect below for new users/signing in
+app.post('/user', createNewUser)
+app.post('/signin', signIn)
 export default app
