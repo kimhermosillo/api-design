c93a42f55746e02b3a7e877a43b03c0f320d1d0b
Kim Hermosillo <kimhermosillo@yahoo.com>
2024-11-17 23:43:52 -0700

    updated product handlers and migrated db

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -26,5 +26,43 @@ export const getOneProduct = async (req, res) => {
 }
 
 export const createProduct = async (req, res) => {
+    const product = await prisma.product.create({
+        data: {
+            name: req.body.name,
+            // belongsToId: req.user.id,
+            belongsTo: req.user.belongsTo
 
+        }
+    })
+
+    res.json({ data: product })
+}
+
+export const updateProduct = async (req, res) => {
+    const update = await prisma.product.update({
+        where: {
+            id: req.params.id,
+            belongsToId: req.user.id
+        },
+        data: {
+            name: req.body.title,
+        }
+    })
+
+    res.json({ data: update })
+}
+
+export const deleteProduct = async (req, res) => {
+    const deleted = await prisma.product.delete({
+        where: {
+            // id: req.params.id,
+            // belongsToId: req.user.id,
+            id_belongsToId: {
+                id: req.params.id,
+                belongsToId: req.user.id
+            }
+        },
+    })
+
+    res.json({ data: deleted })
 }
